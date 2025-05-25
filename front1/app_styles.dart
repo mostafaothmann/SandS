8d2b1d4f8fa8c39cf9876670a88880f80364a63a
Tayma18 <163421351+Tayma18@users.noreply.github.com>
2025-05-25 23:23:34 +0300

    Create app_styles.dart

---
+++ front1/app_styles.dart
@@ -0,0 +1,74 @@
+
+import 'package:flutter/material.dart';
+
+
+class AppColors {
+  static const primary = Color.fromRGBO(55, 9, 110, 1);
+  static const secondary = Color(0xFFED7D31);
+  
+  static const accent = Colors.black54;
+   static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color grey = Colors.grey;
+}
+
+
+class AppFonts {
+  static const String family = 'Cairo';
+
+  static const TextStyle subTitleunderline = TextStyle(
+    fontFamily: family,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    decoration: TextDecoration.underline,
+  );
+
+  static const TextStyle bigTitleunderline = TextStyle(
+    fontFamily: family,
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    decoration: TextDecoration.underline,
+  );
+  
+  static const TextStyle subsub = TextStyle(
+    fontFamily: family,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+   
+  );
+  static const TextStyle subtitlebold = TextStyle(
+    fontFamily: family,
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+   
+  );
+  static const TextStyle bigtitlebold = TextStyle(
+    fontFamily: family,
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+   
+  );
+  static const TextStyle bigtitle = TextStyle(
+    fontFamily: family,
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+   
+  );
+  static const TextStyle subtitle = TextStyle(
+    fontFamily: family,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+   
+  );
+  static const TextStyle titless = TextStyle(
+    fontFamily: family,
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+   
+  );
+
+
+
+
+}
+
